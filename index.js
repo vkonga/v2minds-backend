057db1dba061c53b816c831984999f056e15dc5c
venkatesh kongara <92284701+vkonga@users.noreply.github.com>
2024-07-28 09:56:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post("/notes", async (request, response) => {
         // Execute the query
         const notes = await db.run(addNotes);
         // Respond with a success message
-        response.send("Notes Add Successfully");
+        response.send(notes);
     } catch (e) {
         // Handle errors during insertion
         response.send("Adding Note Unsuccessful");
